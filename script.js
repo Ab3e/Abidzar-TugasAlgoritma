5244066843f324db66d258142aeee3fe43ea1bca
Ab3e <144750307+Ab3e@users.noreply.github.com>
2023-11-07 14:58:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let BeliRumah = parseInt(prompt("Masukkan Harga Rumah?"));
+
+if (BeliRumah >= "100" ){
+    alert("Terimakasih telah membeli rumah BAGUS dan silahkan mengisi biodata tersebut")
+    document.write("TERIMAKASIH BANYAK")
+}
+else {
+    alert("Terimakasih Telah membeli rumah tersebut dan silahkan isi biodata tersebut")
+    document.write("TERIMAKASIH SEMOGA DUIT KEKUMPUL!!")
+}
